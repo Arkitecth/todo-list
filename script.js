49b0a63048fb67a9d83abb23804f4760e5af2dd2
Trent_T <trenttuedon2000@gmail.com>
2023-07-04 13:07:50 -0400

    Save state functionality

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ const dragDropParagraph = document.querySelector(".drag-drop");
 const mobileStatusBar = document.querySelector(".status.mobile"); 
 
 
+let listItems; 
 let numElements = 0; 
+let id = 0; 
 let draggedTarget = null; 
 let mouseMovement = 0; 
 let mouseDirection = null; 
@@ -69,40 +71,44 @@ function updateUI(listItem, inputText, checkbox) {
         inputText.classList.add("dark-list-color");
         checkbox.classList.add("dark-check");
     }
+
+    
 }
 
 function updateNumElements() {
     numElementsLeft.textContent = numElements; 
 }
 
-
-
-function deleteItem(listItem) {
-    if(numElements === 1) {
-        listGroup.style.display = "none";
-        statusBar.style.display = "none";
-        dragDropParagraph.style.display = "none";
+function onLoad() {
+    let listItems = JSON.parse(localStorage.getItem("listItems")); 
+    let listItem; 
+    if (listItems != null) {
+        listItems.forEach((object) => {
+            listItem = createItem(object.item); 
+            updateState(listItem, listItem.firstElementChild, object.completed)
+            onChecked();
+        })
     }
-    numElements -= 1; 
-    listItem.remove(); 
-    updateNumElements(); 
-}
 
-function hideCross(listItem) {
-    listItem.currentTarget.lastElementChild.style.display = "none";
-}
-
-function showCross(listItem) {
-    listItem.currentTarget.lastElementChild.style.display = "inline";
 }
 
 
-function addTodo(value) {
-    // Create a Todo Element 
-    // Div
+function updateIndex() {
+    id = 0;
+    let listGroupItems = document.querySelectorAll(".list-group .list-item"); 
+    listGroupItems.forEach((element) => {
+        element.setAttribute("data-id", id); 
+        id += 1; 
+    })
+}
+
+
+function createItem(value) {
     const listItem = document.createElement("div"); 
     listItem.setAttribute("draggable", true)
     listItem.classList.add("list-item")
+    listItem.setAttribute("data-id", id)
+    id += 1;
 
     // Checkbox 
     const checkbox = document.createElement("input"); 
@@ -146,7 +152,6 @@ function addTodo(value) {
         ev.preventDefault();
     })
     
-
     listItem.addEventListener("drop", (ev) => {
         ev.preventDefault(); 
         if(mouseDirection === "up") {
@@ -155,18 +160,87 @@ function addTodo(value) {
         else if(mouseDirection === "down") {
             listGroup.insertBefore(draggedTarget, listItem.nextElementSibling);
         }
-      
     })
-
     updateUI(listItem, inputEntry, checkbox);
     updateNumElements(); 
-   
+
+    return listItem;
+
 }
 
+function addTodo(value) {
+    createItem(value);
+    addItemToStorage(value);
+}
+
+function addItemToStorage(value) {
+    listItems = getItemFromStorage()
+    listItems.push({item:value, completed: false}); 
+    localStorage.setItem("listItems", JSON.stringify(listItems)); 
+}
+
+function getItemFromStorage() {
+    if(localStorage.getItem("listItems") === null) {
+        listItems = []
+    } else {
+        listItems = JSON.parse(localStorage.getItem("listItems"))
+    }
+    return listItems
+}
+
+
+
+function deleteItem(listItem) {
+    let listItems = JSON.parse(localStorage.getItem("listItems"));
+    if(numElements === 1) {
+        listGroup.style.display = "none";
+        statusBar.style.display = "none";
+        dragDropParagraph.style.display = "none";
+    }
+    numElements -= 1;
+    updateIndex();
+    listItems.splice(listItem.dataset.id, 1);
+    localStorage.setItem("listItems", JSON.stringify(listItems))
+    listItem.remove(); 
+    updateNumElements(); 
+}
+
+function hideCross(listItem) {
+    listItem.currentTarget.lastElementChild.style.display = "none";
+}
+
+function showCross(listItem) {
+    listItem.currentTarget.lastElementChild.style.display = "inline";
+}
+
+function updateState(listItem, button, completed) {
+    if(completed) {
+        // Update State 
+        listItem.classList.add("complete");
+        button.click(); 
+        button.nextElementSibling.classList.add("complete");
+        button.nextElementSibling.setAttribute("disabled", true);
+    } else {
+        listItem.classList.remove("complete");
+        button.nextElementSibling.classList.remove("complete");
+        button.nextElementSibling.removeAttribute("disabled");
+    }
+
+}
+
+
 function checkButtonHandler(element) {
-    element.currentTarget.parentElement.classList.toggle("complete");
+    let listItems = getItemFromStorage(); 
+    let listItem =  element.currentTarget.parentElement;
+    listItem.classList.toggle("complete");
     element.currentTarget.nextElementSibling.classList.toggle("complete");
     element.currentTarget.nextElementSibling.toggleAttribute("disabled");
+    if(listItems[listItem.dataset.id].completed) {
+        listItems[listItem.dataset.id].completed = false
+    } else {
+        listItems[listItem.dataset.id].completed = true
+    }
+    localStorage.setItem("listItems", JSON.stringify(listItems));
 }
 
 function onChecked() {
@@ -238,6 +312,8 @@ filterActive.forEach((element) => {
 filterCompleted.forEach((element) => {
     element.addEventListener("click", showComplete);
 })
+
+document.addEventListener("DOMContentLoaded", onLoad); 
 
 document.addEventListener("mousemove", (e) => {
     if(e.clientY > mouseMovement) {
